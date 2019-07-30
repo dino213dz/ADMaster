afafac3c7505cd0304756dd00185ace447d5d433
dino213dz <h4ckr213dz@gmail.com>
2019-07-30 22:39:04 +0200

    Version 1.0 : Projet testé en production

---
+++ README.md
@@ -0,0 +1,38 @@
+# DESCRIPTION:
+
+ - Outil permettant d'effectuer les fréquentes tâches de maintenance rapidement.
+ - Tous les résultats sont exportables en csv.
+ - Ce script possède un menu et des raccourcis
+
+# UTILISATION:
+ - Le script doit être lancé depuis la ligne de commande powershell.
+ - Le module Powershell AD dit être disponibles.
+ - Il faut donc lancer le script depuis un serveur AD avec les droits administrateurs
+
+ - Exemple:
+    - ADMaster.ps1
+
+# FLEXIBILITE
+ - L'outil est entierrement parametrable sans éditer le code principal
+ - Il suffit d'éditer le fichier "modules/cfg.ps1" 
+ - Il est possible d'ajouter d'autres scripts et leurs fonctions facilement.
+ - Ajouter des catégories
+ - Modifier les couleurs de l'interface
+ - Modifier les messages affichés à l'ecran
+
+
+ - Ajouter une categorie:
+   - Pour ajouter une catégorie de menus (ensemble de liens)
+     - Ouvrir le fichier "modules/cfg.ps1" 
+     - Editer la variable "$admaster_categories" et ajoutez une entrée au tableau
+     - Ajoutez votre entree à la fin du tableau derniere case pour ne pas trop casser l'agencement: ("entrée","entree2","Entree3"..."Votre entree")
+   - Remarque:
+     - L'ordre des categries est important car il sert d'index aux liens. La premiere etant le n° zero
+
+ - Ajouter un lien:
+     - Ouvrir le fichier "modules/cfg.ps1" 
+     - Editer la variable "$admaster_liens" et ajoutez une entrée au tableau
+     - Chaque entrée est un tableau également : @("Titre du lien", numero_index_categorie, 'NomFonction' )
+   - Remarque:
+     - L'ordre des categories des liens est important. Chaque lien affiche sa caategorie si le lien précedent n'est pas de la meme categorie.
+     - Si vous souhaitez avoir une seule categorie suivie de ses liens, il est necessaire que les categories se suivent.
---
+++ modules/csv.ps1
@@ -0,0 +1,50 @@
+﻿
+function Export-CSV ($liste, $fichier_destination) {    
+    $liste | export-csv "$fichier_destination"
+}
+
+
+function Ask-Export-CSV ([array] $liste_a_exporter) {  
+    [int] $nb_lignes=$liste_a_exporter.count+1
+    
+    #$liste_a_exporter=$($liste_a_exporter|format-table|ConvertTo-Csv)
+    Show-Messages "Exporter ces résultats vers un fichier CSV ($nb_lignes lignes)?" 'titre' $true
+    $verif=$(Ask-OuiNon " |_[?]  Tapez <oui> pour confirmer : " $col_soustitre $fnd_soustitre)
+    
+    if ($verif){
+        
+        Write-host "`n[+] Fichier destination :" -ForegroundColor $col_titre -BackgroundColor $fnd_titre
+        Write-host " |_[-] Chemin (Laissez vide pour annuler) : " -ForegroundColor $col_soustitre -BackgroundColor $fnd_soustitre -NoNewline
+        $fichier_csv_destination=read-host
+        
+        if ($fichier_csv_destination.length -eq 0){
+            Show-Messages "`n[+] Export annulé!" 'titre' $false
+            return
+            }
+        
+        Write-host "`n[+] Export :" -ForegroundColor $col_titre -BackgroundColor $fnd_titre
+        Write-host " |_[-] Destination : " -ForegroundColor $col_soustitre -BackgroundColor $fnd_soustitre -NoNewline
+        Write-host "$fichier_csv_destination" -ForegroundColor $col_warning -BackgroundColor $fnd_default       
+        
+
+        try {
+            #write-host " |_[i] info : " -ForegroundColor $col_soustitre -NoNewline
+            #write-host "Fonction d'export en cours de dévelopement!" -ForegroundColor $col_warning
+            [array] $array_csv = $liste_a_exporter|ConvertTo-Csv -NoTypeInformation -Delimiter ";"
+            foreach ($ligne in $array_csv){
+                Write-Output $ligne >> $fichier_csv_destination
+                }
+            notepad $fichier_csv_destination
+            }
+        catch {
+            $erreur=$_.Exception.Message
+            $erreur=$erreur -replace "Could not find a part of the path","Verifiez le chemin. Un dossier doit manquer"
+            write-host " |_[X] erreur : " -ForegroundColor $col_soustitre -NoNewline
+            write-host "$erreur" -ForegroundColor $col_erreur
+            }
+        Show-Messages "`n[+] Export terminé!" 'titre' $false
+        }
+    else {
+        Show-Messages "`n[+] Export annulé!" 'titre' $false
+        }
+}
